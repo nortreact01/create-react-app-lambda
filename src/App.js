592c7a85d816a395a85e6ec607c9d3394c524aa8
Andres Järviste <andres.jarviste@wbscodingschool.com>
2023-02-28 07:56:50 +0200

    esimene

--- src/App.js
+++ src/App.js
@@ -40,6 +40,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            Kes hommikuti külas käib, see asjatult ei longi
+          </p>
           <LambdaDemo />
         </header>
       </div>
